9bb021ac2f72167f78f36b84fb0ef2a8bc3ea6f6
gash94 <104354207+gash94@users.noreply.github.com>
2023-06-02 19:47:06 +0200

    add function routing

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,48 @@
-const express = require('express')
+const express = require("express");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const contactsFunctions = require("../../models/contacts");
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const checkContactId = (contact, contactId, res) => {
+    if (!contact) {
+        return res
+            .status(404)
+            .json({ message: `Contact with id=${contactId} was not found.` });
+    }
+};
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res, next) => {
+    const contacts = await contactsFunctions.listContacts();
+    res.json(contacts);
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+    const { contactId } = req.params;
+    const contact = await contactsFunctions.getContactById(contactId);
+    checkContactId(contact, contactId, res);
+    res.json(contact);
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+    const newContact = await contactsFunctions.addContact(req.body);
+    res.status(201).json(newContact);
+});
 
-module.exports = router
+router.delete("/:contactId", async (req, res, next) => {
+    const { contactId } = req.params;
+    const contact = await contactsFunctions.removeContact(contactId);
+    checkContactId(contact, contactId, res);
+    return res.status(200).json({
+        message: `Contact with id=${contactId} was deleted.`,
+    });
+});
+
+router.put("/:contactId", async (req, res, next) => {
+    const { contactId } = req.params;
+    const contact = await contactsFunctions.updateContact(contactId, req.body);
+    checkContactId(contact, contactId, res);
+    return res.status(200).json(contact);
+});
+
+module.exports = router;
