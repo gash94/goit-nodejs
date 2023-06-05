11de5694ab25ec7a0be2ad279910a7c38ed3b3cd
gash94 <104354207+gash94@users.noreply.github.com>
2023-06-05 18:20:36 +0200

    add try catch

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,8 +1,10 @@
 const express = require("express");
-
 const router = express.Router();
-
 const contactsFunctions = require("../../models/contacts");
+const {
+    validateCreateContact,
+    validateUpdateContact,
+} = require("../../models/validator");
 
 const checkContactId = (contact, contactId, res) => {
     if (!contact) {
@@ -25,24 +27,50 @@ router.get("/:contactId", async (req, res, next) => {
 });
 
 router.post("/", async (req, res, next) => {
-    const newContact = await contactsFunctions.addContact(req.body);
-    res.status(201).json(newContact);
+    try {
+        const { error } = validateCreateContact(req.body);
+        if (error) {
+            return res.status(400).json({ message: error.message });
+        }
+        const newContact = await contactsFunctions.addContact(req.body);
+        res.status(201).json(newContact);
+    } catch (error) {
+        next(error);
+        return res.status(500).json({ message: "Server error" });
+    }
 });
 
 router.delete("/:contactId", async (req, res, next) => {
-    const { contactId } = req.params;
-    const contact = await contactsFunctions.removeContact(contactId);
-    checkContactId(contact, contactId, res);
-    return res.status(200).json({
-        message: `Contact with id=${contactId} was deleted.`,
-    });
+    try {
+        const { contactId } = req.params;
+        const contact = await contactsFunctions.removeContact(contactId);
+        checkContactId(contact, contactId, res);
+        return res.status(200).json({
+            message: `Contact with id=${contactId} was deleted.`,
+        });
+    } catch (error) {
+        next(error);
+        return res.status(500).json({ message: "Server error" });
+    }
 });
 
 router.put("/:contactId", async (req, res, next) => {
-    const { contactId } = req.params;
-    const contact = await contactsFunctions.updateContact(contactId, req.body);
-    checkContactId(contact, contactId, res);
-    return res.status(200).json(contact);
+    try {
+        const { contactId } = req.params;
+        const { error } = validateUpdateContact(req.body);
+        if (error) {
+            return res.status(400).json({ message: error.message });
+        }
+        const contact = await contactsFunctions.updateContact(
+            contactId,
+            req.body
+        );
+        checkContactId(contact, contactId, res);
+        return res.status(200).json(contact);
+    } catch (error) {
+        next(error);
+        return res.status(500).json({ message: "Server error" });
+    }
 });
 
 module.exports = router;
