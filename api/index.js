3de8d8be2dbe46a0780a9daf3780e56318978bdb
gash94 <104354207+gash94@users.noreply.github.com>
2023-06-15 20:27:45 +0200

    add routing, auth to fix

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const ctrlContact = require("../controller");
 
-router.get("/contacts", ctrlContact.get);
+router.get("/contacts", ctrlContact.auth, ctrlContact.get);
 
 router.get("/contacts/:id", ctrlContact.getById);
 
@@ -13,5 +13,9 @@ router.put("/contacts/:id", ctrlContact.update);
 router.patch("/contacts/:id/favorite", ctrlContact.updateStatus);
 
 router.delete("/contacts/:id", ctrlContact.remove);
+
+router.post("/users/signup", ctrlContact.registration);
+
+router.post("/users/login", ctrlContact.login);
 
 module.exports = router;
