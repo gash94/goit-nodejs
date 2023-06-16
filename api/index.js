91f0aa18a624c6f87b7b04edcaf6267e38232420
gash94 <104354207+gash94@users.noreply.github.com>
2023-06-16 17:25:46 +0200

    update routing and src function

--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const ctrlContact = require("../controller");
+const ctrlContact = require("../controller/ctrlContact");
+const ctrlUser = require("../controller/ctrlUser");
+const ctrlAuth = require("../controller/auth");
 
-router.get("/contacts", ctrlContact.auth, ctrlContact.get);
+router.get("/contacts", ctrlAuth.auth, ctrlContact.get);
 
 router.get("/contacts/:id", ctrlContact.getById);
 
@@ -14,8 +16,12 @@ router.patch("/contacts/:id/favorite", ctrlContact.updateStatus);
 
 router.delete("/contacts/:id", ctrlContact.remove);
 
-router.post("/users/signup", ctrlContact.registration);
+router.post("/users/signup", ctrlUser.registration);
 
-router.post("/users/login", ctrlContact.login);
+router.post("/users/login", ctrlUser.login);
+
+router.get("/users/logout", ctrlAuth.auth, ctrlUser.logout);
+
+router.get("/users/current", ctrlAuth.auth, ctrlUser.current);
 
 module.exports = router;
